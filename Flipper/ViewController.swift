641307b40991b0fb149c1233205cfb91971ee0d8
Ethan Alvey <alveyeb@s.dcsdk12.org>
2019-12-09 07:30:02 -0700

    Initial Commit

---
+++ Flipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flipper
+//
+//  Created by Ethan Alvey on 12/9/19.
+//  Copyright © 2019 Fractyl Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
